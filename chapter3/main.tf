a0b841cf139e05c9093d20785921e26da3b2afd6
Masahiko Funaki <mfunaki@mayoct.net>
2025-01-06 17:30:54 +0900

    Update .gitignore and add main.tf for AWS instance configuration

---
+++ chapter3/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.41.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_instance" "test_server" {
+    ami = "ami-0eba6c58b7918d3a1"
+    instance_type = "t2.micro"
+
+    tags = {
+      Name = "TestInstance"
+    }
+  
+}
